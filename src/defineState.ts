669157e96753ab748137ac93836988fcc5abdb74
Martin Ramm <martin-ramm@gmx.de>
2023-02-10 12:01:31 +0100

    Bugfix: Require _onEnter handler if state requires arguments on transition

--- src/defineState.ts
+++ src/defineState.ts
@@ -1,9 +1,13 @@
 import type {Fsm} from "./fsm";
 import type {SpecialEventNames} from "./SpecialEventNames";
 import type {EventFn} from "./EventFn";
+import type {GetStateArguments} from "./state";
 
 export type DefineState<F extends Fsm, StateName extends keyof F["states"]> = {
     [eventName in SpecialEventNames | keyof F["events"]]?: EventFn<F, eventName, StateName>
-};
+} &
+    [] extends GetStateArguments<F["states"][StateName]>
+        ? {}
+        : {_onEnter: EventFn<F, '_onEnter', StateName>};
 
 export const defineState = <F extends Fsm, StateName extends keyof F["states"]>(handlers: DefineState<F, StateName>) => handlers
