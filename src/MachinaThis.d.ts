e32302d700e8c71cce39c2b1c3150e0c3a769d96
Martin Ramm <martin-ramm@gmx.de>
2023-02-11 00:41:18 +0100

    Add event emitter functions (custom and internal events)

--- src/MachinaThis.d.ts
+++ src/MachinaThis.d.ts
@@ -2,15 +2,21 @@ import {Fsm} from "./fsm";
 import {SpecialEventNames} from "./SpecialEventNames";
 import {EventFn} from "./EventFn";
 import {GetEventArguments} from "./event";
+import {InternallyEmittedEvents} from "./InternallyEmittedEvents";
+
+type EventHandlerFn<F extends Fsm, EventName extends '*' | keyof InternallyEmittedEvents<F> | keyof F["emittableEvents"]> =
+    EventName extends keyof InternallyEmittedEvents<F>
+        ? (this: MachinaThisInitializeFn<F> | MachinaThis<F>, ...args: GetEventArguments<InternallyEmittedEvents<F>[EventName]>) => any
+        : EventName extends keyof F["emittableEvents"]
+            ? (this: MachinaThisInitializeFn<F> | MachinaThis<F>, ...args: GetEventArguments<F["emittableEvents"][EventName]>) => any
+            : (this: MachinaThisInitializeFn<F> | MachinaThis<F>, eventName: keyof InternallyEmittedEvents<F> | keyof F["emittableEvents"], ...data: GetEventArguments<InternallyEmittedEvents<F>[keyof InternallyEmittedEvents<F>]> | GetEventArguments<F["emittableEvents"][keyof F["emittableEvents"]]>) => any;
 
 export type MachinaThisInitializeFn<F extends Fsm> = {
-    readonly initialize: F extends Fsm<any, any, any, infer ConstructorArguments, infer IsInitializeFnAdded>
+    readonly initialize: F extends Fsm<any, any, any, any, infer ConstructorArguments, infer IsInitializeFnAdded>
         ? IsInitializeFnAdded extends true
             ? (this: MachinaThisInitializeFn<F>, ...args: ConstructorArguments) => any
             : never
         : never,
-    //TODO add functions of fsm object (handle, on, etc...)
-    //TODO add user defined functions of fsm object
     readonly states: {
         [state in keyof F["states"]]: {
             [event in keyof F["defaultHandlers"]]: EventFn<F, event>
@@ -25,7 +31,11 @@ export type MachinaThisInitializeFn<F extends Fsm> = {
     readonly hierarchy: object,
     readonly pendingDelegations: object,
 
-    transition<StateName extends keyof F["states"]>(stateName: StateName, ...args: GetStateArguments<F["states"][StateName]>): void;
+    transition<StateName extends keyof F["states"]>(stateName: StateName, ...args: GetStateArguments<F["states"][StateName]>): void,
+    handle<EventName extends keyof F["events"]>(eventName: EventName, ...args: GetEventArguments<F["events"][EventName]>): void,
+    emit<EventName extends keyof F["emittableEvents"]>(eventName: EventName, ...args: GetEventArguments<F["emittableEvents"][EventName]>): void,
+    on<EventName extends '*' | keyof InternallyEmittedEvents<F> | keyof F["emittableEvents"]>(eventName: EventName, fn: EventHandlerFn<F, EventName>): void,
+    off<EventName extends '*' | keyof InternallyEmittedEvents<F> | keyof F["emittableEvents"]>(eventName: EventName, fn: EventHandlerFn<F, EventName>): void,
 } & F["userDefinedFunctions"];
 
 export type CurrentActionArgs<F extends Fsm, EventName extends keyof F["events"] = keyof F["events"]> =
@@ -34,12 +44,14 @@ export type CurrentActionArgs<F extends Fsm, EventName extends keyof F["events"]
         ...GetEventArguments<F["events"][EventName]>
     ];
 
+export type CurrentAction<F extends Fsm> = '' | `${keyof F["states"] extends string | number ? keyof F["states"] : never}.${keyof F["events"] extends string | number ? keyof F["events"] : never}`;
+
 export type MachinaThis<F extends Fsm> = MachinaThisInitializeFn<F> & {
     readonly inputQueue: any[],
     readonly targetReplayState: keyof F["states"],
     readonly state: keyof F["states"],
     readonly priorState: '' | keyof F["states"],
-    readonly currentAction: '' | `${keyof F["states"] extends string | number ? keyof F["states"] : never}.${keyof F["events"] extends string | number ? keyof F["events"] : never}`,
+    readonly currentAction: CurrentAction<F>,
     readonly currentActionArgs: undefined | CurrentActionArgs<F>,
     readonly inExitHandler: boolean,
 };
--- src/fsm.ts
+++ src/fsm.ts
@@ -10,39 +10,46 @@ export type Fsm<
     States extends {[key: string]: State} = any,
     Events extends {[key: string]: Event} = any,
     DefaultHandlers extends {[key: string]: EventFn} = any,
+    EmittableEvents extends {[key: string]: Event} = any,
     ConstructorArguments extends any[] = any,
     IsInitializeFnAdded extends boolean = any,
     UserDefinedFunctions extends {[key: string]: GenericUserFunction} = any,
 > = {
     readonly states: States,
     readonly events: Events,
+    readonly emittableEvents: EmittableEvents,
     readonly defaultHandlers: DefaultHandlers,
     readonly userDefinedFunctions: UserDefinedFunctions,
-    readonly initializeFn: IsInitializeFnAdded extends false ?  undefined : ((this: MachinaThisInitializeFn<Fsm<States, Events, DefaultHandlers, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>>, ...args: ConstructorArguments) => any),
-    addDefaultHandler:
+    readonly initializeFn: IsInitializeFnAdded extends false ?  undefined : ((this: MachinaThisInitializeFn<Fsm<States, Events, DefaultHandlers, EmittableEvents, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>>, ...args: ConstructorArguments) => any),
+    readonly addDefaultHandler:
         <EventName extends Exclude<SpecialEventNames | keyof Events, keyof DefaultHandlers>>
-        (eventName: EventName, fn: EventFn<Fsm<States, Events, DefaultHandlers, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>, EventName>)
-            => Fsm<States, Events, DefaultHandlers & Record<EventName, typeof fn>, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>,
-    addInitializeFn: IsInitializeFnAdded extends true ? never :
+        (eventName: EventName, fn: EventFn<Fsm<States, Events, DefaultHandlers, EmittableEvents, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>, EventName>)
+            => Fsm<States, Events, DefaultHandlers & Record<EventName, typeof fn>, EmittableEvents, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>,
+    readonly addInitializeFn: IsInitializeFnAdded extends true ? never :
         <NewConstructorArguments extends any[]>
-        (fn: (this: MachinaThisInitializeFn<Fsm<States, Events, DefaultHandlers, NewConstructorArguments, true, UserDefinedFunctions>>, ...args: NewConstructorArguments) => any)
-            => Fsm<States, Events, DefaultHandlers, NewConstructorArguments, true, UserDefinedFunctions>,
-    addUserDefinedFn:
+        (fn: (this: MachinaThisInitializeFn<Fsm<States, Events, DefaultHandlers, EmittableEvents, NewConstructorArguments, true, UserDefinedFunctions>>, ...args: NewConstructorArguments) => any)
+            => Fsm<States, Events, DefaultHandlers, EmittableEvents, NewConstructorArguments, true, UserDefinedFunctions>,
+    readonly addUserDefinedFn:
         <FnName extends string,
         Fn extends
             FnName extends keyof UserDefinedFunctions
                 ? 'Function with given name already defined'
                 : FnName extends keyof MachinaThis<Fsm> | 'deferUntilTransition' | 'deferAndTransition'
                     ? 'Function name is a used by machina'
-                    : GenericUserFunction<Fsm<States, Events, DefaultHandlers, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>>>
+                    : GenericUserFunction<Fsm<States, Events, DefaultHandlers, EmittableEvents, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions>>>
         (name: FnName, fn: Fn)
-            => Fsm<States, Events, DefaultHandlers, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions & Record<FnName, typeof fn>>,
+            => Fsm<States, Events, DefaultHandlers, EmittableEvents, ConstructorArguments, IsInitializeFnAdded, UserDefinedFunctions & Record<FnName, typeof fn>>,
 };
 
 export const fsm = <
     States extends {[key: string]: State},
-    Events extends {[key: string]: Event}
->(param: {states: States, events: Events}): Fsm<States, Events, {}, [], false, {}> => ({
+    Events extends {[key: string]: Event},
+    EmittableEvents extends {[key: string]: Event} = {},
+>(param: {
+    states: States,
+    events: Events,
+    emittableEvents: EmittableEvents,
+}): Fsm<States, Events, {}, EmittableEvents, [], false, {}> => ({
     ...param,
     defaultHandlers: {},
     initializeFn: undefined,
