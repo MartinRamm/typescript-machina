c6425d55f98bb2a605e50ef7bad6388520748e06
Martin Ramm <martin-ramm@gmx.de>
2023-02-11 23:39:38 +0100

    Test tsc compatibility and specify correct peerDependency

---
+++ test/tsc-version-compatibility.js
@@ -0,0 +1,22 @@
+const { spawn } = require("child_process");
+
+fetch('https://registry.npmjs.org/typescript')
+    .then(response => response.json())
+    .then(json => [
+        json['dist-tags']['beta'],
+        json['dist-tags']['next'],
+        ...Object.keys(json.versions).filter(version => {
+            if (version.match(/^[0-9]+(\.[0-9]+){2}$/)) {
+                const [major, minor] = version.split('.').map(num => Number.parseInt(num));
+                return major > 4 || (major === 4 && minor >= 4);
+            }
+            return false;
+        }).sort().reverse()
+    ])
+    .then(versions => versions.map(version => new Promise(resolve => {
+        spawn('pnpm', `--package typescript@${version} dlx tsc --noEmit --project tsconfig.json`.split(' '))
+            .on("close", code => resolve([version, code === 0]));
+    })))
+    .then(promises => Promise.all(promises))
+    .then(entries => Object.fromEntries(entries))
+    .then(console.log);
