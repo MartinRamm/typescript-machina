9dc3ff3ca2eeb7159ea0b848f32b37982d5da2b0
Martin Ramm <martin-ramm@gmx.de>
2023-02-13 23:45:19 +0100

    Add interface to project

---
+++ src/fsm.ts
@@ -0,0 +1,5 @@
+export { builder } from './builder';
+export { state } from './state';
+export { event } from './event';
+export { handler } from './handler';
+export { defineState } from './defineState';
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * as fsm from './fsm';
