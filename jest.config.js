87984c915b6e604f19c0bb61e17e0b8302d8c550
Martin Ramm <martin-ramm@gmx.de>
2023-02-14 13:42:25 +0100

    setup jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
+  coverageReporters: [],
+  collectCoverage: true,
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: ['.d.ts$'],
   testMatch: ['<rootDir>/test/unit/**/*.ts'],
   globalSetup: './test/unit-setup/setup.ts',
 
