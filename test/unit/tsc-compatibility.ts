51b151bfe81601bd9ce818f7628b3476d3ef493a
Martin Ramm <martin-ramm@gmx.de>
2023-02-16 00:50:27 +0100

    on error log output of child process, to enable debugging

--- test/unit/tsc-compatibility.ts
+++ test/unit/tsc-compatibility.ts
@@ -2,27 +2,34 @@ import { describe, expect, test } from '@jest/globals';
 import { spawn } from 'child_process';
 import { getTscVersions } from '../unit-setup/getTscVersions';
 
-describe('tsc-version-compatibility', () => {
+describe('typescript version compatibility', () => {
   getTscVersions().forEach(version =>
-    test.concurrent(version, async () => {
-      const exitCode = await new Promise(resolve => {
-        const child = spawn(
-          'pnpm',
-          `--package typescript@${version} dlx tsc --noEmit --project tsconfig.json 2>&1`.split(' ')
-        );
+    test.concurrent(
+      version,
+      async () => {
+        const exitCode = await new Promise(resolve => {
+          const child = spawn(
+            'pnpm',
+            `--package typescript@${version} dlx tsc --noEmit --project tsconfig.json`.split(' ')
+          );
 
-        let stdout = '';
-        child.stdout.on('data', line => (stdout += `\n${JSON.stringify(line)}`));
+          let stdout = '';
+          //eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          child.stdout.on('data', line => (stdout += `\n${JSON.stringify(line.toString())}`));
+          //eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          child.stderr.on('data', line => (stdout += `\n${JSON.stringify(line.toString())}`));
 
-        child.on('close', code => {
-          if (code !== 0) {
-            console.log('tsc-version-compatibility', version, stdout);
-          }
-          resolve(code);
+          child.on('close', code => {
+            if (code !== 0) {
+              console.log('tsc version compatibility', version, stdout);
+            }
+            resolve(code);
+          });
         });
-      });
 
-      expect(exitCode).toBe(0);
-    })
+        expect(exitCode).toBe(0);
+      },
+      60 * 1_000
+    )
   );
 });
