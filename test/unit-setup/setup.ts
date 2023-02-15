e72e52caf189065dc5d7d15bbd60355175ed5db0
Martin Ramm <martin-ramm@gmx.de>
2023-02-15 20:29:21 +0100

    On error display stdout and stderr when testing tsc versions

--- test/unit-setup/setup.ts
+++ test/unit-setup/setup.ts
@@ -1,4 +1,6 @@
 import { setupTscVersions } from './getTscVersions';
 
+//this file is run by jest on setup --> see ../../jest.config.js["globalSetup"]
+
 //eslint-disable-next-line import/no-default-export
 export default () => Promise.all([setupTscVersions()]);
--- test/unit/tsc-compatibility.ts
+++ test/unit/tsc-compatibility.ts
@@ -6,10 +6,20 @@ describe('tsc-version-compatibility', () => {
   getTscVersions().forEach(version =>
     test.concurrent(version, async () => {
       const exitCode = await new Promise(resolve => {
-        spawn('pnpm', `--package typescript@${version} dlx tsc --noEmit --project tsconfig.json`.split(' ')).on(
-          'close',
-          code => resolve(code)
+        const child = spawn(
+          'pnpm',
+          `--package typescript@${version} dlx tsc --noEmit --project tsconfig.json 2>&1`.split(' ')
         );
+
+        let stdout = '';
+        child.stdout.on('data', line => (stdout += `\n${JSON.stringify(line)}`));
+
+        child.on('close', code => {
+          if (code !== 0) {
+            console.log('tsc-version-compatibility', version, stdout);
+          }
+          resolve(code);
+        });
       });
 
       expect(exitCode).toBe(0);
